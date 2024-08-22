57f393637d7a91307a6052060ec7bf0bb8419c2f
gael55x <gailleamolong5@gmail.com>
2024-08-22 23:07:01 +0800

    C-001: Fixed pyramid

---
+++ full-pyramid.c
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+
+int main(void) { 
+    int i, k, rows; 
+
+    for(i = 0; i < k; i++) {
+        
+    }
+}
--- left-pyramid.c
+++ left-pyramid.c
@@ -1,20 +1,20 @@
 // C lang code that generates a left pyramid (basically half lmao)
-#include <stio.h> 
+#include <stdio.h> 
 
-int main() {
+int main(void) {
     int i, j, rows;
 
     // ask user for no. of rows
-    printf("Enter the no. of rows: ");
-    scanf("%d", rows);
-
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+    
     // generate left-aligned pyramid
     for (i = 1; i < rows; i++){
         for (j = 1; j < i; j++) {
-            print('#');
+            printf("#");
         }
         // Move to next line after row '#' prints
-        printf('\n');
+        printf("\n");
     }
     return 0;
 }
