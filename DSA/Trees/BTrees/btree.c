b24fb3226f81f95646cc9291a68a2612528856fd
gael55x <gailleamolong5@gmail.com>
2025-03-21 22:51:24 +0800

    initialized btree files

---
+++ DSA/Trees/BTrees/btree.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+typedef struct Node{
+    struct Node * left; 
+    int val; 
+    struct Node * right; 
+}
+
+void * create(){
+    Node * new = (Node*)malloc(sizeof(struct Node)); 
+
+}
+
+
+int main(){
+    
+}
